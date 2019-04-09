b5d0f38280d6e95f4d291b28eca33be7649df451
capntechbeard <capntechbeard@gmail.com>
2019-04-09 11:38:55 -0500

    React Fragments and Picsum

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
   render() {
-    return <h1>Greetings Earthlings</h1>;
+    return (
+      <React.Fragment>
+        <img src="this.state.imageUrl" alt="" />
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 

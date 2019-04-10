39faabf8777ae946756d1c6c4b61fdfb07f5dd4b
capntechbeard <capntechbeard@gmail.com>
2019-04-10 15:48:34 -0500

    React Style typo

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span styles={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>

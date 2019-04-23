1e8a5954f5251f95c9bdf9f83c362caa7c3664ca
capntechbeard <capntechbeard@gmail.com>
2019-04-23 14:40:32 -0500

    React Developer Tools & Deleting Entries

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,17 +4,38 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
   };
+  handleReset = () => {
+    this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset{" "}
+        </button>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id} />
+          <Counter
+            key={counter.id} //internal
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
